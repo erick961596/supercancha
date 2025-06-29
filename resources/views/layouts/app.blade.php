43d8d2b4024525515de544ef0eac0aefa769a1e9
Interaction <51757797+servicios-terceros@users.noreply.github.com>
2025-06-28 19:58:12 -0600

    primera carga

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <!-- Scripts de Breeze -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+ 
     <style>
         :root {
             --primary-color: #00B873;
